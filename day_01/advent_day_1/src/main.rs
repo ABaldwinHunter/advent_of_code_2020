6f160329b539878c7bdeaf1766d774625b02f721
Ashley Baldwin-Hunter <ashley.baldwinhunter@betterment.com>
2020-12-01 23:30:52 -0500

    Use correct file

--- day_01/advent_day_1/src/main.rs
+++ day_01/advent_day_1/src/main.rs
@@ -2,9 +2,9 @@ use std::fs;
 
 fn main() {
     // --snip--
-    println!("In file {}", "../../input.txt");
+    println!("In file {}", "../input.txt");
 
-    let contents = fs::read_to_string("../.../input.txt")
+    let contents = fs::read_to_string("../input.txt")
         .expect("Something went wrong reading the file");
 
     println!("With text:\n{}", contents);
