4a8b570bb9714b59a71daa46f78f4f4239a9d740
Ashley Baldwin-Hunter <ashley.baldwinhunter@betterment.com>
2020-12-02 01:09:05 -0500

    Brute force solution

--- day_01/solution.rb
+++ day_01/solution.rb
@@ -23,8 +23,41 @@ end
 # Found them! 861 and 1159
 # product is 997899
 
-first_third = nums.select { |n| n <= 700 }
 
-nums.each.with_index do |num, index|
+# thirds
+#
+
+two_thirds = 673 * 2 # 1346
+
+first_two_thirds = nums.select { |n| n <= two_thirds }
+first_two_third_couples = {}
+
+first_two_thirds.dup.each.with_index do |num, index|
+  first_two_thirds.each.with_index do |n, i|
+    if i != index
+      key = [i, index].sort.map(&:to_s).join("_")
+
+      if first_two_third_couples[key].nil?
+        first_two_third_couples[key] = n + num
+      end
+    end
+  end
 end
 
+rest = nums.select { |n| n > two_thirds }
+
+nums.each.with_index do |num, index|
+  first_two_third_couples.each do |key, sum|
+    if (num + sum) == target
+      indices = key.split("_").map(&:to_i)
+
+      unless indices.include? index
+        puts "Found three: #{num}, #{nums[indices.first]}, #{nums[indices.last]}"
+        puts "product is #{(num * nums[indices.first] * nums[indices.last])}"
+      end
+    end
+  end
+end
+
+# Found three: 1406, 277, 337
+# product is 131248694
