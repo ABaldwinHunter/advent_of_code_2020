983b917a12566e288cfe586cc860b78d96964506
Ashley Baldwin-Hunter <ashley.baldwinhunter@betterment.com>
2020-12-01 23:28:53 -0500

    Playing with rust

---
+++ day_01/advent_day_1/target/debug/advent_day_1.d
@@ -0,0 +1,1 @@
+/Users/ashleybaldwinhunter/src/advent_of_code_2020/day_01/advent_day_1/target/debug/advent_day_1: /Users/ashleybaldwinhunter/src/advent_of_code_2020/day_01/advent_day_1/src/main.rs
---
+++ day_01/advent_day_1/target/debug/deps/advent_day_1.d
@@ -0,0 +1,5 @@
+/Users/ashleybaldwinhunter/src/advent_of_code_2020/day_01/advent_day_1/target/debug/deps/advent_day_1: src/main.rs
+
+/Users/ashleybaldwinhunter/src/advent_of_code_2020/day_01/advent_day_1/target/debug/deps/advent_day_1.d: src/main.rs
+
+src/main.rs:
