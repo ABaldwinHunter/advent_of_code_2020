064282b156d1d9b9ebf147a8d444297af27cd060
Ashley Baldwin-Hunter <ashley.baldwinhunter@betterment.com>
2020-12-03 00:47:21 -0500

    Part one day two complete

---
+++ day_02/solution.rb
@@ -0,0 +1,28 @@
+rows = File.read("./input.txt").split("\n")
+
+items = rows.map { |row| row.split(" ") }.map do |item|
+  item[0] = item[0].split("-").map(&:to_i) # range
+  item[1] = item[1].split(":").first
+  item[2] = item[2]
+  item
+end
+
+def valid?(range, letter, password)
+  occurrences = password.count(letter)
+
+  if (occurrences >= range.first and occurrences <= range.last)
+    true
+  else
+    false
+  end
+end
+
+valid_count = 0
+
+items.each do |item|
+  if valid?(item[0], item[1], item[2])
+    valid_count += 1
+  end
+end
+
+puts "Valid count is #{valid_count}"
