c4464a24ad3402e5813e4d2f9be19f7ea8cd8735
Ashley Baldwin-Hunter <ashley.baldwinhunter@betterment.com>
2020-12-10 00:43:06 -0500

    WIP

--- day_10/solution.rb
+++ day_10/solution.rb
@@ -28,3 +28,34 @@ sorted.each do |adapter|
 end
 
 puts "One jolt differences = #{one_jolt_differences}. Three jolt: #{three_jolt_differences}. Product: #{(one_jolt_differences * three_jolt_differences)}"
+
+# Part II combinatorics
+#
+# Whenever there is one that can be switched out it is like an extra input to a combinatorics
+#
+
+# (0), 1, 4, 5, 6, 7, 10, 11, 12, 15, 16, 19, (22)
+# (0), 1, 4, 5, 6, 7, 10, 12, 15, 16, 19, (22)
+# (0), 1, 4, 5, 7, 10, 11, 12, 15, 16, 19, (22)
+# (0), 1, 4, 5, 7, 10, 12, 15, 16, 19, (22)
+# (0), 1, 4, 6, 7, 10, 11, 12, 15, 16, 19, (22)
+# (0), 1, 4, 6, 7, 10, 12, 15, 16, 19, (22)
+# (0), 1, 4, 7, 10, 11, 12, 15, 16, 19, (22)
+# (0), 1, 4, 7, 10, 12, 15, 16, 19, (22)
+
+fixed_segments = []
+dynamic_segments  = []
+
+# array.combination
+#
+# multiply the number of possibilities in the dynamic sections
+
+current_segment = []
+segment_start = 0 # include start and end of previous segment
+latest_segment_adapter = 0
+
+sorted.each.with_index do |adapter, index|
+  diff = adapter - latest_segment_adapter
+  if diff <= 3 # continue segment
+
+end
