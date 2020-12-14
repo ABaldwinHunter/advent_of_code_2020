7f056c0ef540a07b45c8409060aeaac80ba4a834
Ashley Baldwin-Hunter <ashley.baldwinhunter@betterment.com>
2020-12-14 01:51:53 -0500

    Day 13 part 2 wip

--- day_11/day_13/solution.rb
+++ day_11/day_13/solution.rb
@@ -4,7 +4,7 @@ time = input.first.to_i
 
 buses = input.last.split(",").reject { |bus| bus == 'x' }.map { |bus| bus.to_i }
 
-answer =  nil
+answer = nil
 
 i = 0
 
@@ -20,4 +20,34 @@ while answer.nil?
   i += 1
 end
 
+puts "answer is #{answer}"
+
+# part II
+
+inputs = "17,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,643,x,x,x,x,x,x,x,23,x,x,x,x,13,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,29,x,433,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,x,x,19".split(",").map do |item|
+  if item == "x"
+    item
+  else
+    item.to_i
+  end
+end
+
+# num % 17 = 0
+# (num + 7) % 41 = 0
+# 41 - 7 = 34
+
+# number should be divisible by 34, and 17
+#
+factors = []
+
+inputs.each.with_index do |item, index|
+  if item == "x"
+    next
+  else
+    factors << (item)
+    factors << (item + index)
+  end
+end
+
+answer = factors.inject(1) { |product,x| x * product }
 puts "answer is #{answer}"
