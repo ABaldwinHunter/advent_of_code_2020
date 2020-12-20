826ab4b2a479d9212869932103481b05793aebff
Ashley Baldwin-Hunter <ashley.baldwinhunter@gmail.com>
2020-12-19 23:53:08 -0500

    Day 16 part II WIP

--- day_16/solution.rb
+++ day_16/solution.rb
@@ -61,3 +61,40 @@ num_hash.each do |key, occurrences|
 end
 
 puts "odd duck count is #{odd_duck_count}"
+
+# Part II
+
+invalid_numbers = []
+
+num_hash.each do |key, occurrences|
+  if simplified.none? { |tuple| key >= tuple.first && key <= tuple.last }
+    invalid_numbers << key
+  end
+end
+
+rules = sections.first
+
+my_ticket = sections[1]
+
+nearby_tickets = nearby_ticket_section.split("\n")
+nearby_tickets.shift # get rid of "nearby tickets:"
+
+arrays_of_nums = nearby_tickets.map { |str| str.split(",").map { |num| num.to_i } }
+valid_tickets = arrays_of_nums.reject { |ticket| ticket.any? { |num| invalid_numbers.include?(num) } }
+
+puts "valid ticket count is #{valid_tickets.count}"
+
+rules_hash = {}
+
+rules.split("\n").map do |str|
+  parts = str.split(": ")
+  field = parts.first
+
+  ranges_part = parts.last
+
+  unparsed_ranges = ranges_part.split(" or ")
+
+  ranges = unparsed_ranges.map { |range| range.split("-").map { |num| num.to_i } }
+
+  rules_hash[field] = ranges
+end
